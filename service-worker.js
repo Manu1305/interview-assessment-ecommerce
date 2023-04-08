090a3287e1c4cc75f4e1c4186c709f139f5a801f
Manu <manukrishnan1305@gmail.com>
2023-04-08 10:38:20 +0530

    second commit

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open('v1').then(function (cache) {
+            return cache.addAll([
+                '.',
+                
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function (cacheName) {
+                    return cacheName.startsWith('v') && cacheName !== 'v1';
+                }).map(function (cacheName) {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        })
+    );
+});
